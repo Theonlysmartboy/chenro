455a39d74535c729ff1fbe4407fbbb08a1071c5c
TheOnlySmartBoy <o2jose43@gmail.com>
2017-09-28 19:31:08 +0300

    Variable declaration

--- src/com/otema/tosby/timetabler/cs/Data.java
+++ src/com/otema/tosby/timetabler/cs/Data.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.otema.tosby.timetabler.cs;
 
-/**
- *
- * @author MASENO
- */
+import com.otema.tosby.timetabler.cs.domain.Course;
+import com.otema.tosby.timetabler.cs.domain.Department;
+import com.otema.tosby.timetabler.cs.domain.Instructor;
+import com.otema.tosby.timetabler.cs.domain.MeetingTime;
+import com.otema.tosby.timetabler.cs.domain.Room;
+import java.util.ArrayList;
+
 public class Data {
-    
+
+    private ArrayList<Room> rooms;
+    private ArrayList<Instructor> intstructors;
+    private ArrayList<Course> courses;
+    private ArrayList<Department> depts;
+    private ArrayList<MeetingTime> meetingtimes;
+    private int numberOfClasses = 0;
+
+    private Data() {
+        initialize();
+    }
+
+    private Data initialize() {
+        return this;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public ArrayList<Instructor> getIntstructors() {
+        return intstructors;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public ArrayList<Department> getDepts() {
+        return depts;
+    }
+
+    public ArrayList<MeetingTime> getMeetingtimes() {
+        return meetingtimes;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
 }
