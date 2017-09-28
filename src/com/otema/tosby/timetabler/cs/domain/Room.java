86647f60227ce88bd110a6a101caf82e9d265cbd
TheOnlySmartBoy <o2jose43@gmail.com>
2017-09-28 17:11:18 +0300

    Variable declaration

--- src/com/otema/tosby/timetabler/cs/domain/Room.java
+++ src/com/otema/tosby/timetabler/cs/domain/Room.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.otema.tosby.timetabler.cs.domain;
 
-/**
- *
- * @author MASENO
- */
 public class Room {
+    private String number;
+    private int SeatingCApacity;
+
+    public Room(String number, int SeatingCApacity) {
+        this.number = number;
+        this.SeatingCApacity = SeatingCApacity;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getSeatingCApacity() {
+        return SeatingCApacity;
+    }
     
 }
