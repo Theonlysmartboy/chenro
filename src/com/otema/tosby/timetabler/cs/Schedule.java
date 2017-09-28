b44e615eb88e8f9e631b35413c69ee2a3169852d
TheOnlySmartBoy <o2jose43@gmail.com>
2017-09-28 17:18:57 +0300

    Variable declaration

--- src/com/otema/tosby/timetabler/cs/Schedule.java
+++ src/com/otema/tosby/timetabler/cs/Schedule.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.otema.tosby.timetabler.cs;
 
-/**
- *
- * @author MASENO
- */
+import java.util.ArrayList;
+
+
 public class Schedule {
+    private ArrayList<Class> classes;
     
 }
