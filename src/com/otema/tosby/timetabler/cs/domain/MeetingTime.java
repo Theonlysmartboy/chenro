d57180e2d6d2b1757e4e6f17eb89ec84f1a7268d
TheOnlySmartBoy <o2jose43@gmail.com>
2017-09-28 17:08:30 +0300

    Variable declaration

--- src/com/otema/tosby/timetabler/cs/domain/MeetingTime.java
+++ src/com/otema/tosby/timetabler/cs/domain/MeetingTime.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.otema.tosby.timetabler.cs.domain;
 
-/**
- *
- * @author MASENO
- */
 public class MeetingTime {
+    private String Id;
+            private String time;
+
+    public MeetingTime(String Id) {
+        this.Id = Id;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+            
     
 }
