c9d6e86a1fbe11a6a129063e7271be76458a4eee
TheOnlySmartBoy <o2jose43@gmail.com>
2017-09-28 17:25:00 +0300

    Variable declaration

--- src/com/otema/tosby/timetabler/cs/domain/Class.java
+++ src/com/otema/tosby/timetabler/cs/domain/Class.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.otema.tosby.timetabler.cs.domain;
 
-/**
- *
- * @author MASENO
- */
 public class Class {
+    private int Id;
+    private Department dept;
+    private Course course;
+    private Instructor instructor;
+    private MeetingTime meetingtime;
+    private Room room;
+
+    public Class(int Id, Department dept, Course course) {
+        this.Id = Id;
+        this.dept = dept;
+        this.course = course;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setMeetingtime(MeetingTime meetingtime) {
+        this.meetingtime = meetingtime;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public MeetingTime getMeetingtime() {
+        return meetingtime;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
     
 }
