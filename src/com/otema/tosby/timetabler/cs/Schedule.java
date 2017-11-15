452f591f1ebf81dac000a096a588b3f69ba1d946
TheOnlySmartBoy <o2jose43@gmail.com>
2017-11-15 12:06:28 +0300

    Began working on the Schedule file

--- src/com/otema/tosby/timetabler/cs/Schedule.java
+++ src/com/otema/tosby/timetabler/cs/Schedule.java
@@ -6,5 +6,15 @@ import java.util.ArrayList;
 
 public class Schedule {
     private ArrayList<Class> classes;
+    private Data data;
+    public Data getData(){
+        return data;
+    }
+
+    public Schedule(Data data) {
+        this.data = data;
+        classes = new ArrayList<Class>(data.getNumberOfClasses());
+    }
+    
     
 }
